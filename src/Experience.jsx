5a2c9b39dca661243e620848d4cb34e915b98f6b
jhon suescun <104960124+SuescunJhon@users.noreply.github.com>
2023-09-03 19:02:12 -0500

    Box implementing
    se creo un mesh de un cubo con meshStandarMaterial que rota a una velocidad delta.

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,8 +1,20 @@
-const Experience = ({title, info}) => {
+import {OrbitControls} from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import {useRef} from "react";
+
+const Experience = () => {
+    const boxRef = useRef();
+    useFrame((state, delta) => {boxRef.current.rotation.x += 1 * delta});
+
     return(<>
-            <h2 className="title"> {title} </h2>
-            <span> {info} </span>
-        </>
+        <OrbitControls makeDefault />
+        <ambientLight intensity={0.5}/>
+        <directionalLight position={[10, 10, 5]} intensity={2}/>
+        <mesh ref={boxRef}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color="yellow"/>
+        </mesh>
+    </>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import {createRoot} from "react-dom/client";
 import Experience from "./Experience";
 import "./styles.css";
+import {Canvas} from "@react-three/fiber";
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-    <Experience 
-    title={'Portfolio - Jhon Suescun'}
-    info={'Web 3D Developer'}/>
+    <Canvas camera={{position: [2, 0, 5]}}>
+        <Experience/>
+    </Canvas>
 );
