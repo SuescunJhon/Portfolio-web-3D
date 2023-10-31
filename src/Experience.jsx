52439d98dbe6c3ef89e7457d4c5ee797cdbdf430
jhon suescun <104960124+SuescunJhon@users.noreply.github.com>
2023-10-31 18:10:36 -0500

    se añadieron luces y sombras

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,21 +1,30 @@
-import {OrbitControls} from "@react-three/drei";
+import { OrbitControls, SpotLight } from "@react-three/drei";
 import Fox from "./World/Fox";
 import Suelo from "./World/Suelo";
 import Pared from "./World/Pared";
 import Lights from "./World/Lights";
 import Environment from "./World/Environment";
+import Sphere from "./World/Sphere";
+import Cube from "./World/Cube";
+import Cone from "./World/Cone";
+import Torus from "./World/Torus";
 
 const Experience = () => {
-    return(
-    <>
-        <OrbitControls makeDefault/>
-        <Lights/>
-        <Environment/>
-        <Fox position={[5,1.5,5]} scale={0.6} rotation-y={5}/>
-        <Suelo/>
-        <Pared position={[5, 2, 0]} args={[10,8]}/>
-        <Pared position={[10,2,5]} args={[10,8]} rotation={[0, -Math.PI/2, 0]}/>
-    </>
+
+    return (
+        <>
+            <OrbitControls makeDefault />
+            <Lights />
+            <Environment />
+            <Sphere position={[-3, 0, 1]} castShadow />
+            <Cube position={[-1, 0, 3]} castShadow />
+            <Cone position={[.5, 0, 5]} castShadow />
+            <Torus position={[2, 0, 7]} rotation={[0, -.78, 0]} />
+            <Fox position={[5, 1.5, 5]} scale={0.6} rotation-y={5} />
+            <Suelo position={[5, -2, 5]} rotation={[-Math.PI / 2, 0, -Math.PI / 4]} receiveShadow />
+            <Pared position={[9, 2, 2]} args={[20, 8]} rotation={[0, -0.78, 0]} receiveShadow />
+            <Pared position={[12, 2, 12]} args={[10, 8]} rotation={[0, -2.35, 0]} receiveShadow />
+        </>
     );
 }
 
--- src/World/Suelo.jsx
+++ src/World/Suelo.jsx
@@ -1,18 +1,16 @@
 import { useTexture } from "@react-three/drei";
 
-const Suelo = () => {
+export default function Suelo(props) {
     const PATH = "/assets/textures/wood/"
     const propsTexture = useTexture({
         map: PATH + "laminate_floor_03_diff_2k.jpg",
-        
+
     })
 
     return (
-        <mesh  position={[5, -2, 5]}  rotation-x={-Math.PI / 2} receiveShadow>
-            <planeGeometry attach="geometry" args={[10,10]} />
-            <meshStandardMaterial {...propsTexture}/>
-        </mesh> 
-    )    
+        <mesh {...props}>
+            <planeGeometry args={[20, 10]} />
+            <meshStandardMaterial {...propsTexture} />
+        </mesh>
+    );
 }
-
-export default Suelo;
